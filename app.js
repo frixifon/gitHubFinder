460bd6c2d8f57dcf4c50250fc7652289cef28f2a
frixifon <38159256+frixifon@users.noreply.github.com>
2019-10-08 15:21:02 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,73 @@
+//Init Github Class
+const github = new Github;
+//Init UI class
+const ui = new UI;
+
+const searchUser = document.getElementById('searchUser');
+
+searchUser.addEventListener('keyup', (event) => {
+    //get input text from the searchUser input field
+    const userText = event.target.value;
+    if(userText !== ''){
+            //make an http call to github API
+            github.getUser(userText)
+            .then(data =>{
+                if(data.profile.message === 'Not Found') {
+                    //Show alert if user was not found 
+                    ui.showAlert('User Not Found', 'alert alert-danger');
+                } else{
+                    //Show user profile and repos
+                    ui.showProfile(data.profile);
+                    ui.showRepos(data.repos);
+                }
+                console.log(data);
+            })
+    } else{
+        ui.clearProfile();
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*loadEventListeners();
+
+function loadEventListeners() {
+    document.addEventListener(`DOMContentLoaded`, getObject);
+}
+
+function getObject(){
+    const gitHub = new Github;
+    console.log(gitHub);
+
+    gitHub.getUser(`Sempiternus666`)
+    .then(data=>{
+        console.log(data);
+    })
+}*/
